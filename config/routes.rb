c5c167dcdb8933c656a933d4615091735c857a14
Yilin Chen <wormful@gmail.com>
2013-12-14 20:11:47 +0800

    Finish Index pge

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Gamball::Application.routes.draw do
+  get "static/index"
+  resources :users
+
+  root 'static#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
