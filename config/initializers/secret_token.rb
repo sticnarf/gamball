ac0cf502cd68cc036670ea1802f2a9c5221746ae
Yilin Chen <wormful@gmail.com>
2013-12-14 16:27:55 +0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gamball::Application.config.secret_key_base = '1509b2dbad241c22232577fe1cd1beafbfa8136e99c4e1137aaec0c97dede401c35dcfd5d3af0ff1ea58b96321ca403293c4215d45c133e34b9153d258e1f79e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gamball::Application.config.session_store :cookie_store, key: '_gamball_session'
